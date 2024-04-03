2bb71be09252786c08c62326ce964c48d46b2211
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-04-03 16:32:02 +0800

    project setup

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="quicksand">this is quicksand</div>
+      <div className="source-sans-3">this is source sans</div>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "light-pink": "#FFD4D4",
+        "dark-green": "#AACB73",
+        "light-green": "#CDE990",
+        "beige-white": "#FFFFE8",
+        "black": "#1E1E1E",
+        "yellow" : "#F2E5A6",
+        "dark-yellow" : "#EBDFA4",
+        "text-yellow": "#AEA05E",
+        "blue" : "#BDFFFB",
+        "dark-blue" : "#70A29F",
+        "text-blue" : "#A0E9E4",
+        "red" : "#FFC0C4",
+        "dark-red" : "#EDACB1",
+        "text-red" : "#9E6D70",
+      },
+    },
+  },
+  plugins: [],
+};
