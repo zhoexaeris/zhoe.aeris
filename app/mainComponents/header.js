6587decb2bbac05490b5384d44ad0eda0ae85609
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-04-03 22:53:53 +0800

    updated home page

--- app/mainComponents/header.js
+++ app/mainComponents/header.js
@@ -4,25 +4,28 @@ import { TypeAnimation } from 'react-type-animation'
 
 const Tagline = () => {
   return (
-    <TypeAnimation
+     <TypeAnimation
       cursor={true}
-      sequence={['Zhoe Aeris', 1000,
-                'a UI/UX Designer', 1000,
-                'a Web Developer', 1000,
-                'a Dog Lover', 1000]}
+      sequence={['An aspiring UI/UX Designer from De La Salle University Manila.', 1000]}
       repeat={Infinity}
+      className='lg:text-left sm:pr-5'
     />
   )
 }
 
 const Header = ({color}) => {
   return (
-    <section className={`flex flex-col flex-wrap w-full items-center py-10 h-screen`}>
-      <div className='quicksand font-extrabold text-5xl w-full items-center text-center'>
-        Hi, I am <span className='text-[#D88080]'>Zhoe Aeris!</span>
+    <section className={`flex flex-col flex-wrap w-full items-center py-10 h-screen sm:flex-row lg:px-64 text-[#D88080]`}>
+      <div className='sm:flex-1 w-full items-center text-center'>
+        <div className='quicksand font-extrabold text-5xl px-5 lg:text-left lg:px-0'>
+            Hi, I am <span className='text-[#9F6361]'>Zhoe Aeris</span>
+        </div> 
+        <div className='text-xl lg:text-left'>
+            <Tagline />
+        </div>
       </div>
-      <div className='flex w-full items-center justify-center py-5'>
-        <img src="images/me.jpg" className='w-64 rounded-full shadow-3xl'/>
+      <div className='flex w-full items-center justify-center py-5 sm:flex-1 px-5'>
+        <img src="images/me.jpg" className='w-64 rounded-full shadow-3xl lg:w-96'/>
       </div>
     </section>
   )
--- app/page.js
+++ app/page.js
@@ -3,12 +3,12 @@ import Header from "./mainComponents/header";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#F9F5F6] ">
+    <main className="flex min-h-screen flex-col bg-[#E7CBCB] ">
       <div className="bg-red w-full">
-        <Navbar color="bg-red" />
+        <Navbar color="bg-[#E7CBCB]" />
       </div>
       <div className="body">
-        <Header color="red" />
+        <Header color="bg-[#E7CBCB]" />
       </div>
       <div className="load color bg-red hidden"> h</div>
     </main>
