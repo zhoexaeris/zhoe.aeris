136be9ec7b64424622b6763d0c547fb2ddf80d6f
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-04-04 03:49:36 +0800

    added footer

---
+++ app/mainComponents/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+    const linkedIn = <FontAwesomeIcon icon={faLinkedin} className="w-5 h-5"/>
+    const email = <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5"/>
+    const github = <FontAwesomeIcon icon={faGithub} className="w-5 h-5"/>
+
+    return (
+        <footer className='source-sans-3 py-2 w-full text-center bg-white'>
+        Designed and coded by Zhoe Aeris
+        <br />
+        <ul className='flex justify-center py-1'>
+            <li className='text-black px-1'><a href="https://www.linkedin.com/in/zhoeaeris/" target="_blank">{linkedIn}</a></li>
+            <li className='text-black px-1'><a href="/" target="_blank">{email}</a></li>
+            <li className='text-black px-1'><a href="https://www.github.com/zhoexaeris/" target="_blank">{github}</a></li>
+        </ul>
+        </footer>
+    )
+}
+
+export default Footer
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Navbar from "./mainComponents/navbar.js";
 import Header from "./mainComponents/header.js";
+import Footer from "./mainComponents/Footer.js";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <Header color="bg-[#E7CBCB]" />
       </div>
       <div>
-        
+        <Footer />
       </div>
       <div className="load color bg-red hidden"> h</div>
     </main>
