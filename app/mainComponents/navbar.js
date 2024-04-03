8d3a5171ac49e86e94527981e6334757472a06f0
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-04-03 18:52:29 +0800

    added responsive navbar

---
+++ app/mainComponents/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar }  from '@fortawesome/free-regular-svg-icons';
+
+const Navbar = ({color}) => {
+    const starIcon = <FontAwesomeIcon icon={faStar} className="w-7 h-7"/>
+    return (
+        <nav className={`w-full ${color} px-5 py-3 items-center text-xl quicksand sm:flex lg:px-64`}>
+            <div className="flex justify-center w-full p-3 sm:flex-1">
+                <Link href={"/"}>{starIcon}</Link>
+            </div>
+            <div className="flex justify-center w-full sm:justify-end ">
+                <ul className='text-center font-semibold sm:flex sm:items-center'>
+                    <li className='hover:underline'><Link href={"/"}>Projects</Link></li>
+                    <li className='hover:underline pl-3'><Link href={"/about"}>About</Link></li>
+                    <li className='pl-3 hover:underline'><a href={"https://drive.google.com/file/d/1J60ZguzLY0pJHCPzgJjatSVbvxgU9pGG/view?usp=sharing" } target="_blank">Resume</a></li>
+                </ul>
+
+            </div>
+        </nav>
+    )
+    {/* TODO: add a feature that scrolls down when Projects is clicked */}
+}
+
+export default Navbar
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Image from "next/image";
+import Navbar from "./mainComponents/navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col bg-[#F9F5F6] ">
+      <div className="bg-red w-full">
+        <Navbar color="bg-red" />
+      </div>
       <div className="quicksand">this is quicksand</div>
       <div className="source-sans-3">this is source sans</div>
+      <div className="load color bg-red "> h</div>
     </main>
   );
 }
