68e6460eed006d93698795cd567354bbd7c77bc8
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-04-15 17:49:51 +0800

    added projects

--- app/mainComponents/navbar.js
+++ app/mainComponents/navbar.js
@@ -12,12 +12,12 @@ const Navbar = ({color}) => {
             </div>
             <div className="flex justify-center w-full sm:justify-end">
                 <ul className='text-center font-semibold sm:items-center flex'>
-                    <li className='hover:underline'><Link href={"/projects"}>Projects</Link></li>
+                    <li className='hover:underline pl-5'><Link href={"/projects"}>Projects</Link></li>
                     <li className='hover:underline pl-5'><Link href={"/about"}>About</Link></li>
                     <li className='pl-5 hover:underline'><a href={"https://drive.google.com/file/d/1J60ZguzLY0pJHCPzgJjatSVbvxgU9pGG/view?usp=sharing" } target="_blank">Resume</a></li>
                 </ul>
-
             </div>
+            <hr className="border-[#9F6361] mx-5 my-5"/>
         </nav>
     )
     {/* TODO: add a feature that scrolls down when Projects is clicked */}
