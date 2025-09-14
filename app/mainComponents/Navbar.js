f43398a8a0ba120c59e290f58f43fda5b1a3bce7
Zhoe Aeris <105402733+zhoexaeris@users.noreply.github.com>
2025-09-14 13:45:54 +0800

    chore: updated navbar options

--- app/mainComponents/Navbar.js
+++ app/mainComponents/Navbar.js
@@ -2,16 +2,28 @@ import React from "react";
 
 const Navbar = ({ color }) => {
   return (
-    <nav className={`w-full ${color} flex items-center justify-center py-8`}>
-      <div className="w-full max-w-4xl flex items-center justify-between px-4 mx-auto">
+    <nav className={`w-full ${color} flex items-start justify-center py-8`}>
+      <div className="w-full max-w-4xl flex items-start px-4 mx-auto gap-8">
         <div
           className=" font-bold text-[#231F20]"
           style={{ fontFamily: "Inter, sans-serif" }}
         >
-          Zhoe Aeris
+          Home
         </div>
         <div
-          className="text-base text-[#231F20] font-semibold"
+          className=" font-bold text-[#231F20]"
+          style={{ fontFamily: "Inter, sans-serif" }}
+        >
+          Projects
+        </div>
+        <div
+          className=" font-bold text-[#231F20]"
+          style={{ fontFamily: "Inter, sans-serif" }}
+        >
+          About
+        </div>
+        <div
+          className=" font-bold text-[#231F20]"
           style={{ fontFamily: "Inter, sans-serif" }}
         >
           <a
