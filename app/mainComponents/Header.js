e7e60ad489f0b7778a21fa98ceb4aa533ba05a5a
Zhoe Aeris <105402733+zhoexaeris@users.noreply.github.com>
2025-09-16 17:22:52 +0800

    fix: landing text responsiveness

--- app/mainComponents/Header.js
+++ app/mainComponents/Header.js
@@ -19,18 +19,18 @@ const Header = () => {
 
   return (
     <div className="max-w-4xl">
-      <h1 className="text-3xl font-extrabold text-[#231F20] mb-6">
+      <h1 className="text-xl sm:text-4xl  font-extrabold text-[#231F20] leading-tight tracking-tight mb-6">
         Zhoe is a{" "}
         <span className="bg-gradient-to-r from-pink-500 to-orange-400 bg-clip-text text-transparent">
           Frontend Developer
         </span>
-        , who bridges design and code to
-        <span className="transition-all duration-300 inline-block min-w-[2ch] bg-[#F84B82] text-white px-2 py-1 rounded-md text-center ml-2">
+        , who bridges design and code to{" "}
+        <span className="transition-all duration-300 inline-block min-w-[2ch] bg-[#F84B82] text-white px-2 py-1 rounded-md text-center ">
           {words[currentWordIndex]}
         </span>{" "}
         digital experiences.
       </h1>
-      <p className="text-lg  text-[#231F20] mb-8 max-w-3xl">
+      <p className="text-base sm:text-lg text-[#231F20] mb-8 max-w-3xl">
         Currently, I&apos;m open to new opportunities as I complete my senior
         year at Computer Science at De La Salle University Manila.
       </p>
